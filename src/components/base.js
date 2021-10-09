0a507da37f3b57389ba8a63856c1fc17034c500a
Bayconlife <bayconlife@gmail.com>
2021-10-08 23:28:23 -0700

    base combat system

---
+++ src/components/base.js
@@ -0,0 +1,114 @@
+let finished = false;
+
+const player = {
+	hp: 10,
+	maxHp: 10,
+	attack: 5,
+	speed: 1.1
+};
+
+let enemy = {
+	hp: 10,
+	attack: 2,
+	speed: 2.0
+}
+
+const fight = () => {
+	document.getElementById("fight-button").setAttribute("disabled", "");
+	
+	const bar = document.getElementById("player-attack-bar");
+	const ebar = document.getElementById("enemy-attack-bar");
+
+	finished = false;
+
+	enemy = generateEnemy();
+
+	
+	const playerAttack = () => loop(bar, 0, (player.speed * 1000)/100, () => {
+		enemy.hp = Math.max(enemy.hp - player.attack, 0);
+		update();
+
+		if (enemy.hp <= 0) {
+			return fightFinished();
+		}
+
+		playerAttack();
+	});
+
+	const enemyAttack = () => loop(ebar, 0, (enemy.speed * 1000)/100, () => {
+		player.hp = Math.max(player.hp - enemy.attack, 0);
+		update();
+
+		if (player.hp <= 0) {
+			return fightFinished();
+		}
+
+		enemyAttack();
+	});
+
+	update();
+
+	playerAttack();
+	enemyAttack();
+}
+
+const loop = (bar, progress, interval, onFinish) => {
+	if (finished) {
+		return;
+	}
+
+	bar.style=`width: ${progress}%`;
+	bar.setAttribute("aria-valuenow", progress + "");
+
+	if (progress + 1 > 100) {
+		return onFinish();
+	}
+
+	setTimeout(() => loop(bar, progress + 1, interval, onFinish), interval);
+}
+
+const fightFinished = () => {
+	finished = true;
+
+	if (player.hp <= 0) {
+		document.getElementById("fight-button").hidden = true;
+		document.getElementById("respawn-button").hidden = false;
+		document.getElementById("respawn").hidden = false;
+	} else {
+		document.getElementById("fight-button").removeAttribute("disabled");
+	}
+}
+
+const update = () => {
+	document.getElementById("enemy-hp").innerText = enemy.hp;
+
+
+	document.getElementById("player-hp").innerText = player.hp;
+}
+
+const generateEnemy = () => {
+	return {
+		hp: 10,
+		attack: 10,
+		speed: 2.0
+	}
+}
+
+const respawn = () => {
+	console.log('HALLO')
+	finished = false;
+	loop(document.getElementById("respawn-bar"), 0, 2000/100, () => {
+		console.log('OH MY')
+		finished = true;
+		player.hp = player.maxHp;
+
+		update();
+		
+		document.getElementById("respawn-bar").setAttribute("aria-valuenow", "0");
+		document.getElementById("respawn-bar").style = "width: 0%";
+		document.getElementById("fight-button").hidden = false;
+		document.getElementById("respawn-button").hidden = true;
+		document.getElementById("respawn").hidden = true;
+		document.getElementById("fight-button").removeAttribute("disabled");
+	})
+}
